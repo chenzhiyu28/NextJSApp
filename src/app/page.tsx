bfc620f6f4b5bdb89d76ffb1e3de8901ccc0d8ec
Leopold <czy.leopold97@gmail.com>
2025-06-24 15:06:09 +1000

    add loading for async functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return (<h1>My Homepage</h1>)
+async function getTodos() {
+  await wait(2000);
+  return fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+}
+
+
+export default async function Home() {
+  const todos = await getTodos();
+  return (<h1>{todos.length}</h1>)
+}
+
+
+function wait(duration: number) {
+  return new Promise
+  (resolve => setTimeout(resolve, duration))
 }
