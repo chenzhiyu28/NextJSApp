2f6bd4d0687ccd548cf19b29d2293e322dc3772e
Leopold <czy.leopold97@gmail.com>
2025-06-27 23:18:17 +1000

    modify des

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const metadata: Metadata = {
   title: "About",
-  description: "This is the about page of the app"
+  description: "This is the about page of the app."
 }
 
 export default function page() {
